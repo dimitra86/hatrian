994665665b3526b42b9c768cfdebdbe259120605
Dzmitry Nikalayeu <dimitra86@list.ru>
2024-02-18 19:31:07 +0300

    feat: add config files babel eslint webpack package

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [ "airbnb",
+    "prettier"],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "2017",
+        "parser": "@babel/eslint-parser",
+    },
+    "rules": {
+    }
+}
